42f350c197c7dc53c6c433a0d2975d4d21a82737
Killa <102366446+KillaClick@users.noreply.github.com>
2022-08-26 21:43:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const landRover = document.querySelector(".btn1");
+landRover.addEventListener("click", function () {
+  alert("Ваш заказ набора №42110 успешно оформлен!");
+});
+
+const astonMartin = document.querySelector(".btn2");
+astonMartin.addEventListener("click", function () {
+  alert("Ваш заказ набора № 10262 успешно оформлен!");
+});
+
+const buggati = document.querySelector(".btn3");
+buggati.addEventListener("click", function () {
+  alert("Ваш заказ набора № 42083 успешно оформлен!");
+});
